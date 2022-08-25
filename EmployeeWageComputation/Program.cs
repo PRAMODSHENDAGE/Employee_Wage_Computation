386aaadba5e8180c5f8d54a2d78884b5e06e5569
KajalShendage <shendagekajal123@gmail.com>
2022-08-25 08:54:29 +0530

    [Pramod] Add:Attendence

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using EmployeeWageComputation;
+
+EmployeeWage employeeWage = new EmployeeWage();
+employeeWage.Attendence();
